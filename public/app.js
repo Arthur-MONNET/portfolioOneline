269677bff7aeeb076f869b22f732de2df9412447
Arthur-MONNET <78175924+Arthur-MONNET@users.noreply.github.com>
2021-04-09 10:11:20 +0200

    add public

---
+++ public/app.js
@@ -0,0 +1,28 @@
+let pointer = document.querySelector("#pointer");
+let alla = document.querySelectorAll("a");
+document.addEventListener('mousemove', logKey);
+alla.forEach(a => {
+    a.addEventListener('mouseover', logKeyOver);
+    a.addEventListener('mouseout', logKeyNoOver);
+})
+
+
+document.addEventListener('mouseout', mouseout)
+function logKey(e) {
+    pointer.style.display = `block`;
+    pointer.style.top = `${e.clientY-6}px`;
+    pointer.style.left =`${e.clientX-6}px`;
+}
+function logKeyOver(e) {
+    console.log("over")
+    pointer.style.transform = `scale(2)`;
+    pointer.style.background= "#ec860000";
+}
+function logKeyNoOver(e) {
+    console.log("no over")
+    pointer.style.transform = `scale(1)`;
+    pointer.style.background= "#ec8600";
+}
+function mouseout(e) {
+    pointer.style.display = `none`;
+}
