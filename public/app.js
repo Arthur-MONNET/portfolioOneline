1114d305ef11db36fe0310f6ba3b93762cf40b79
Arthur-MONNET <78175924+Arthur-MONNET@users.noreply.github.com>
2021-05-05 08:40:40 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -117,12 +117,12 @@ window.addEventListener("load", () => {
 
         function logKeyOver(e) {
             pointer.style.transform = `scale(2)`;
-            pointer.style.background = "#f74f1c00";
+            pointer.style.background = "#00000000";
         }
 
         function logKeyNoOver(e) {
             pointer.style.transform = `scale(1)`;
-            pointer.style.background = "#f74f1c";
+            pointer.style.background = "#000000";
         }
 
         function mouseout(e) {
